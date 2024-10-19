ba8e3d9c0267b7b8b5378e954801e05509a2c489
hongtramachiato <94442463+dinhphuckhang@users.noreply.github.com>
2024-10-19 17:44:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-const innerPolygon = document.getElementById("inner-polygon");
-const outer = document.getElementById("outer");
-const closeBtn = document.getElementById("close-btn");
-const message1 = document.getElementById("message-1");
-const message2 = document.getElementById("message-2");
-const heartsRow = document.querySelectorAll(".hearts-row");
-const heartBtn = document.getElementById("heart-btn");
+const card = document.querySelector('.card');
+const openButton = document.querySelector('.open-button');
 
-function toggleEnvelope() {
-  
-  innerPolygon.classList.toggle("inner-open");
-  outer.classList.toggle("outer-open");
-  heartBtn.classList.toggle("hide");
-  closeBtn.classList.toggle("show");
-  message1.classList.toggle("hide");
-  message2.classList.toggle("show");
-  
-  heartsRow.forEach(element => element.classList.toggle("animated"));
-  
-}
+openButton.addEventListener('click', () => {
+  card.classList.toggle('open');
+});
